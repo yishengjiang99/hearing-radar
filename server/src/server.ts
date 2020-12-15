5aa23121e9e096f2708560397ada99cb0f909d8d
Yisheng Jiang <yishengjiang@yahoo.com>
2020-12-15 05:52:37 -0800

    s

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,20 +4,11 @@ import { Oscillator, FileSource, BufferSource } from "./audio-data-source";
 import { SSRContext } from "./ssrctx";
 import { basename, format, resolve } from "path";
 import { Application, Request, Response, Router } from "express";
-import {
-  ReadlineTransform,
-  LSGraph,
-  LSSource,
-  MemoryWritable,
-} from "grep-transform";
+
 import { createReadStream, readFile, readFileSync } from "fs";
 import { convertMidi } from "./sequence";
+import { frequencyToNote, midiToFreq } from "./soundkeys";
 
-let files = [
-  "synth/440/-ac2-f32le.wav",
-  "synth/440/-ac2-s16le.wav",
-  "samples/billie-ac2-ar-44100-s16le.pcm",
-];
 const express = require("express");
 const app: Application = express();
 app.use("*", (req, res, next) => {
@@ -26,18 +17,16 @@ app.use("*", (req, res, next) => {
 });
 
 app.use("/play/:song.mp3", (req, res) => {
-  req.headers.get("if-range") !== null;
   res.writeHead(200, {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "audio/pcm",
     "Content-Disposition": "inline",
   });
-
-  convertMidi("./song.mid", converMp3.stdin, {
+  convertMidi("./song.mid", process.stdout, {
+    mode: "realtime",
     interrupt: process.stdin,
     realtime: true,
   });
-  converMp3.stdout.pipe(res);
 });
 
 app.use("/midisf/:instrument/48000-mono-f32le-(:midi).pcm", (req, res) => {
@@ -46,73 +35,47 @@ app.use("/midisf/:instrument/48000-mono-f32le-(:midi).pcm", (req, res) => {
     "Content-Type": "audio/pcm",
     "Content-Disposition": "inline",
   });
-  let ms;
-
-  const tt = req.query.t || 1;
-
   createReadStream(
     `./midisf/${req.params.instrument}/48000-mono-f32le-${req.params.midi}.pcm`
   ).pipe(res);
 });
+const fpath = (uri) => resolve(__dirname, `../views`, uri);
 
-app.use("/", (req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/html",
-  });
-  res.end(
-    `<html><body>
-      <script>
-
-          const play = async (url)=>{
-            window.ctx = window.ctx || new AudioContext();
-            const  dv=await fetch(url,{
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/pdf'
-              },
-              responseType: "blob"
-          }).then(resp=>resp.blob()).then(blob=>blob.arrayBuffer()).then(ab=>new DataView(ab)).catch(console.log)
-
-              const audb = ctx.createBuffer(1, dv.buffer.byteLength/4, 48000);
-              const buffer =audb.getChannelData(0);
-             for(let i=0; i<audb.length;i++){
-                buffer[i]=dv.getFloat32(i*4,true);
-              }
-              const abs = new AudioBufferSourceNode(ctx, {buffer:audb});
-              abs.connect(ctx.destination);
-              abs.start();
-            
-          }
-      </script>
-      ${execSync("ls -Rm midisf/*/*.pcm")
-        .toString()
-        .trim()
-        .split("\n\n")
-        .map((section) => {
-          if (!section) return;
-          console.log(section);
-          const [instrument, notes] = section.split(":");
-          if (!notes) return;
-          return `<h4>${instrument}<h4>
-          <div>${
-            `` +
-            notes
-              .split(" ")
-              .map((f) => {
-                return `<button><a onclick='play("${f}")'>
-              ${f.split("-").pop().split(".pcm")[0]}</a>
-              </button>`;
-              })
-              .join(" ")
-          }</div>`;
-        })}
-        <playlist>
-       <a href='${req.originalUrl.split("/")[0]}/play/song.mid'>Play MIDI</a>
-        </playlist>
-          
-      </body></html>`
-  );
+app.use("/(:file).js", (req: Request, res: Response) => {
+  console.log(req.params.file);
+  res.sendFile(fpath(req.params.file + ".js"));
 });
+app.use("/", (req, res) => {
+  const sections = execSync("ls -R -m midisf")
+    .toString()
+    .trim()
+    .split("midisf/")
+    .concat(
+      execSync("ls -R -m samples/*pcm").toString().trim().split("samples/")
+    );
+  res.writeHead(200, { contentType: "text/html" });
+  res.write("<html><body>");
+
+  for (const section of sections) {
+    const links = [];
+    res.write("<div>");
+
+    const [instrument, notes] = section.split(":");
+    if (!instrument || !notes) continue;
+    res.write("<h4>" + instrument + "</h4>");
+    notes.split(", ").map((n) => {
+      const nn = n.replace("48000-mono-f32le-", "").replace(".pcm", "");
+      res.write(`<a href="/midisf/${instrument}/${n}"> 
+        ${midiToFreq(nn)} - ${nn} </a>`);
+    });
+    res.write("</div>");
+  }
+  res.write(`<script>
+  ${indexjs}
+  </script>`);
+  res.end("</body></html>");
+});
+
 app.use("/chord/:midi1/:midi2/:midi3/:instrument.mp3", (req, res) => {
   res.writeHead(200, {
     "Access-Control-Allow-Origin": "*",
@@ -171,28 +134,33 @@ app.get("/synth/:freq/:desc", (req, res) => {
   ctx.start();
   ctx.stop(2);
 });
-// const fpath = (uri) => resolve(__dirname, `../views`, uri);
 
-// app.use("/(:file).js", (req: Request, res: Response) => {
-//   console.log(req.params.file);
-//   res.sendFile(fpath(req.params.file + ".js"));
-// });
-// // router.use("/", express.static(resolve(__dirname, "../views/")));
-// const cachedINdex = readFileSync("views/index.html")
-//   .toString()
-//   .split("<!-- data -->");
-
-// const writeSync = async (ws: Writable, str: string) => {
-//   new Promise((resolve) => ws.write(str, resolve));
-// };
-// const pipeSync = async (ws: Writable, rs: Readable) => {
-//   new Promise((resolve) => {
-//     rs.pipe(ws, { end: false });
-//     rs.on("end", resolve);
-//   });
-// };
-// app.use("/", async (req: Request, res: Response) => {
-//   listContainerFiles("pcm").pipe(res);
-// });
+// router.use("/", express.static(resolve(__dirname, "../views/")));
+const cachedINdex = readFileSync("views/index.html")
+  .toString()
+  .split("<!-- data -->");
 
 app.listen(3222, console.log);
+const indexjs = ` 
+document.querySelectorAll("a").forEach(a=>
+  a.onclick= async (e)=>{
+  const url = a.href; 
+  e.preventDefault();
+  globalThis.ctx = globalThis.ctx || new AudioContext();
+  const ctx = globalThis.ctx;
+  const dv = await fetch(url)
+    .then((resp) => resp.blob())
+    .then((blob) => blob.arrayBuffer())
+    .then((ab) => new DataView(ab))
+    .catch(console.log);
+
+  if (!dv) return;
+  const audb = ctx.createBuffer(1, dv.buffer.byteLength / 4, 48000);
+  const buffer = audb.getChannelData(0);
+  for (let i = 0; i < audb.length; i++) {
+    buffer[i] = dv.getFloat32(i * 4, true);
+  }
+  const abs = new AudioBufferSourceNode(ctx, { buffer: audb });
+  abs.connect(ctx.destination);
+  abs.start();
+})`;
